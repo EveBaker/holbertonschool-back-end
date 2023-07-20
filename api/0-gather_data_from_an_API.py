c75357430f6c224f5deccd0d9fab71b886d39cc2
EveBaker <Evebaker0162@gmail.com>
2023-07-20 09:32:44 -0700

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Script: 0-gather_data_from_an_API.py
 Description: This script fetches the TODO list information of an employee from a JSONPlaceholder API 
-based on the given employee ID."""
+based on the given employee ID.
+"""
 import requests
 import sys
 
