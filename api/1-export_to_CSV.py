bd262b0cd1fe51f720ce2ae6ab82d19ad104db02
EveBaker <Evebaker0162@gmail.com>
2023-07-20 17:24:03 -0700

    back end

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,40 +1,33 @@
 #!/usr/bin/python3
 """Script to use a REST API for a given employee ID, returns
-information about his/her TODO list progress"""
+information about his/her TODO list progress and export in CSV"""
 import csv
 import requests
 import sys
 
-API_URL = 'https://jsonplaceholder.typicode.com'
-
-def export_to_csv(employee_id):
-    response_users = requests.get(f"{API_URL}/users/{employee_id}")
-    response_todos = requests.get(f"{API_URL}/todos", params={"userId": employee_id})
-
-    if response_users.status_code !=200 or response_todos.status_code != 200:
-        print(f"Error: unable to retrieve data for employee ID {employee_id}")
+if __name_ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"UsageError:python3 {__file__} employee_id(int)")
         sys.exit(1)
 
-    user_data = response_users.json()
-    todos_data = response_todos.json()
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
 
-    employee_name = user_data.get('name')
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
 
-    with open(f'{employee_id}.csv', 'w', newline='') as csvfile:
-        fieldnames = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
 
-        for task in todos_data:
-            writer.writerow({
-                "USER_ID": employee_id,
-                "USERNAME": employee_name,
-                "TASK_COMPLETED_STATUS": task.get('completed'),
-                "TASK_TITLE": task.get('title')
-            })
+    username = data[0]["user"]["username"]
 
-def main():
-    employee_id = sys.argv[1]
-    export_to_csv(employee_id)
-
-if __name__ == "__main__":
-    main()
+    with open(f"{EMPLOYEE_ID}.csv", "w", newline="") as file:
+        writer = csv.wirter(file, quoting=csv.QUOTE_NONNUMERIC) # Creates a CSV writer object
+        for task in data: # Loops through each task in the received data
+            writer.writerow(
+                    [EMPLOYEE_ID, username, str(task["completed"]), task["title"]]
+            )
