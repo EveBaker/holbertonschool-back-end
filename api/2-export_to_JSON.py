72b51170e3d9e69f580b5821f7de5a84845e0603
EveBaker <Evebaker0162@gmail.com>
2023-07-21 14:45:57 -0700

    backend

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Script to use a REST API for a given employee ID, returns
 information about his/her TODO list progress"""
-import requests
-import sys
 import json
+import sys
+import requests
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
