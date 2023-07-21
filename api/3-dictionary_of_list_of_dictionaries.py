07ee9da82a2047d664b3f5e1cebf71347d0451db
EveBaker <Evebaker0162@gmail.com>
2023-07-21 16:03:16 -0700

    backend

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -8,18 +8,20 @@ import requests
 if __name__ == "__main__":
     API_URL = "https://jsonplaceholder.typicode.com"
 
-    users_response = requests.get(f"{API_URL}/users")
-    users_data = users_response.json()
+    users = requests.get(f"{API_URL}/users").json()
 
-    task_dict = {}
-    for user in users_data:
-        task_list.append({
-            "username": user['username'],
-            "task": task['title'],
-            "completed": task['completed']
-        })
+    dict_users_tasks = {}
+    for user in users:
+        tasks = requests.get(f"{API_URL}/users/{user['id']}/todos").json()
 
-    task_dict[user['id']] = task_list
+        dict_users_tasks[user["id"]] = []
+        for task in tasks:
+            task_dict = {
+                "username": user["username"],
+                "task": task["title"],
+                "completed": task["completed"]
+            }
+            dict_users_tasks[user["id"]].append(task_dict)
 
     with open("todo_all_employees.json", "w") as file:
-        json.dump(task_dict, file)
+        json.dump(dict_users_tasks, file)
